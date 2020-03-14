da200bb637d5e0300528897c31aaf5f91d69cac3
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-03-13 18:16:37 -0700

    added field of view parameter

--- scan_fleet_simulation.py
+++ scan_fleet_simulation.py
@@ -8,38 +8,33 @@ import datetime
 
 from threading import Thread
 from simulation import run, make_fuel_map, ignite_center, state_to_rgb
-from geometry import Space
 
 # to convert the output png files:
 # ffmpeg -i out%03d.png -c:v libx264 -crf 0 -preset veryslow -c:a libmp3lame -b:a 320k output.mp4
-
 
 OUT_DIR_NAME =  f"sim_output_time={str(datetime.datetime.now())}"
 
 if not os.path.isdir(OUT_DIR_NAME):
     os.mkdir(OUT_DIR_NAME)
 
-N_DRONES = 10
+N_DRONES = 100
 N_CELLS = 300
-VELOCITY = 5
-FOV = 5
+VELOCITY = 50
+FOV = 15 # field of view for individual drones
 
 GUI_MARGIN = 0.1
 
 # sin_cos_heading = np.hstack([np.sin(drones_heading), np.cos(drones_heading)])
 # np.sum(np.power(sin_cos_heading,2), axis=1) # sanity check, should be all ones
+np.random.seed(420)
 
 if __name__ == '__main__':
 
     ######################################################################
     # Create Environment
-    fuel = make_fuel_map(N_CELLS, tree_density=0.55)
+    fuel = make_fuel_map(N_CELLS, tree_density=0.6) #tree_density 0.45-0.55 is an interesting case
     wildfire_state = np.zeros_like(fuel) # should the type be dtype=np.bool?
     ignite_center(wildfire_state)
-
-    # define space (just a convinience wrapper for coordinate transforms)
-    # sp = Space(origin=(37.331553, -121.882767)) # south-west corner of campus
-    sp = Space(origin=(37.335404, -121.883400)) # fountain
 
     ######################################################################
     # Simulation
@@ -51,7 +46,7 @@ if __name__ == '__main__':
         'ignition_prob': 0.35,
         'n_epochs': 10,
         'save_dir': None,
-        'loop_min_dur': 0,    
+        'loop_min_dur': 0.1, #throttle down to max 10FPS
         # 'save_dir' = f"sim_output_cells={str(wildfire_state.shape)}_steps={str(sim_args['n_epochs']*sim_args['n_steps'])}_ignition_prob={str(sim_args['ignition_prob'])}_burn_rate={str(sim_args['burn_rate'])}_time={str(datetime.datetime.now())}"
     }
 
@@ -67,6 +62,7 @@ if __name__ == '__main__':
     # Drones !
 
     global_belief = np.zeros_like(wildfire_state)
+    visited = np.zeros_like(wildfire_state)
 
     # init location matrix
     drones_xy = np.random.random((N_DRONES,2)) * N_CELLS
@@ -98,10 +94,9 @@ if __name__ == '__main__':
             x,y = int(x), int(y)
             x = max(0, min(x,N_CELLS-1))
             y = max(0, min(y,N_CELLS-1))
-            # y = min(y,N_CELLS-1)
-            print((x,y))
-            # global_belief[x-5:x+5][y-5:y+5] = wildfire_state[x-5:x+5][y-5:y+5]
-            global_belief[y-FOV:y+FOV,x-FOV:x+FOV] = 1
+
+            global_belief[y-FOV:y+FOV,x-FOV:x+FOV] = wildfire_state[y-FOV:y+FOV,x-FOV:x+FOV]
+            visited[y-FOV:y+FOV,x-FOV:x+FOV] = 255 
 
         # plot stuff
         if gui_iter % 1 == 0:
@@ -117,19 +112,23 @@ if __name__ == '__main__':
             # Belief axis
             axs[0].clear()
             im = state_to_rgb(global_belief, np.zeros_like(global_belief))
+            
+            im = np.stack(3*[global_belief], axis=-1)
             axs[0].imshow(im)
-            axs[0].scatter(drones_xy[:,0], drones_xy[:,1], color='white', s=len(drones_xy[:,0])*[5])
+            axs[0].scatter(drones_xy[:,0], drones_xy[:,1], color='yellow', s=len(drones_xy[:,0])*[5])
             axs[0].set(
                 xlim=(-N_CELLS*GUI_MARGIN, (1+GUI_MARGIN)*N_CELLS),
                 ylim=(-N_CELLS*GUI_MARGIN, (1+GUI_MARGIN)*N_CELLS))
             
             # plot velocity vectors
-            for (x,y),(vx,vy) in zip(drones_xy, drones_velocity_xy):
-                axs[0].arrow(x, y, 3*vx, 3*vy, head_width=0.05, head_length=1, fc='y', ec='y')
+            # for (x,y),(vx,vy) in zip(drones_xy, drones_velocity_xy):
+            #     axs[0].arrow(x, y, 3*vx, 3*vy, head_width=0.05, head_length=.5, fc='y', ec='y')
             
             plt.show(block=False)
             plt.pause(.000001)
-            plt.savefig(f'{OUT_DIR_NAME}/out{gui_iter:03d}.png', dpi=300)
 
+            # We can safe the output (optional)
+            plt.savefig(f'{OUT_DIR_NAME}/out{gui_iter:03d}.png', dpi=300) 
+ 
     sim_thr.join()
     plt.close('all')
